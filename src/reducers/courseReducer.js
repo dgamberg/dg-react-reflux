0bc6e3f84c77890944e3c5ff5e93f24a31c11433
Dave Gamberg <dgamberg@gmail.com>
2016-07-09 12:15:24 -0500

    finished redux and dispatch

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,4 +1,4 @@
-import * as types from './actions/actionTypes';
+import * as types from '../actions/actionTypes';
 //takes the current state and an action and returns a new state
 export default function courseReducer( state = [], action){
   // why we need a type in our action
